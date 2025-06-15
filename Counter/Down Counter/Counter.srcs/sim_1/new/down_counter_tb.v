94b67558a162e15e40bf5a8d6e51270f9fb41d7f
Parthiv Paul <parthivpaul1@gmail.com>
2025-06-15 22:07:11 +0530

    Add files via upload

---
+++ Counter/Down Counter/Counter.srcs/sim_1/new/down_counter_tb.v
@@ -0,0 +1,52 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 28.05.2025 12:27:21
+// Design Name: 
+// Module Name: down_counter_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module down_counter_tb;
+    reg clk, rst, count_en;
+    wire [3:0] q;
+    // Instantiation
+    down_counter uut(
+        .clk(clk),
+        .rst(rst),
+        .count_en(count_en),
+        .q(q)
+        );
+    // Initialization
+    initial begin
+        clk= 1'b0;
+        rst= 1'b0;
+        count_en= 1'b0;
+    end
+    always #5 clk = ~clk;
+    //Varying stimuli
+    initial begin
+        #3 rst = 1'b1;
+        #4 count_en = 1'b1;
+        #170 count_en = 1'b0;
+        #20 rst = 1'b0;
+        #5 $finish;
+    end
+    // Monitoring
+    initial begin
+        $monitor("Time=%t\tclk=%b\trst=%b\tcount_en=%b\tcount=%b\n",$time, clk, rst, count_en, q);
+    end     
+endmodule
