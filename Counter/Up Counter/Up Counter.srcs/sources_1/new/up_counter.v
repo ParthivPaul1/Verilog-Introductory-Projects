9cd2b3a94f2d073629a6b6229ce8c50f93395bd4
Parthiv Paul <parthivpaul1@gmail.com>
2025-06-15 22:08:20 +0530

    Add files via upload

---
+++ Counter/Up Counter/Up Counter.srcs/sources_1/new/up_counter.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 28.05.2025 14:45:24
+// Design Name: 
+// Module Name: up_counter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module up_counter(
+    input clk,
+    input rst,
+    input count_en,
+    output reg [3:0] q
+    );
+    always@(posedge clk or negedge rst) begin
+        if(!rst) q <= 4'b0;
+        else if(count_en) q <= q+1;
+        else q <= q;
+    end
+endmodule
